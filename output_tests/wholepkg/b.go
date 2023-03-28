6ec6094390c176f42c0edba37c6c6af39e39d0ac
Marco Hofstetter <marco.hofstetter@isovalent.com>
2023-03-28 13:40:27 +0200

    deepequal: proper reuse of deepequal method of referenced types
    This change introduces proper reuse of the DeepEqual method when being
    referenced from another type - even if the referenced method gets
    generated in the same generation run.
    
    Without this change reuse miss the occurances which haven't been
    generated yet.
    
    Signed-off-by: Marco Hofstetter <marco.hofstetter@isovalent.com>

--- output_tests/wholepkg/b.go
+++ output_tests/wholepkg/b.go
@@ -5,5 +5,10 @@ Copyright 2016 The Kubernetes Authors.
 
 package wholepkg
 
+import "github.com/cilium/deepequal-gen/output_tests/otherpkg"
+
 // Another type in another file.
-type StructB struct{}
+type StructB struct {
+	OtherWithPrimitivesOnly otherpkg.OtherStructWithPrimitivesOnly
+	OtherWithPointers       otherpkg.OtherStructWithPointers
+}
